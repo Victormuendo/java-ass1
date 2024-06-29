6cb1e5b313d818c118463d4b9ad58b6bc6a2794b
Victormuendo <muendovictor9@gmail.com>
2024-06-29 12:51:21 +0300

    Update JavaApplication1.java

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -1,3 +1,5 @@
+java
+Copy code
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -31,4 +33,78 @@ public class PetDisplayApp extends JFrame {
         petGroup.add(pigButton);
 
         birdButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                displayPet("Bird");
+            }
+        });
+
+        catButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                displayPet("Cat");
+            }
+        });
+
+        dogButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                displayPet("Dog");
+            }
+        });
+
+        rabbitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                displayPet("Rabbit");
+            }
+        });
+
+        pigButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                displayPet("Pig");
+            }
+        });
+
+        JPanel radioPanel = new JPanel(new GridLayout(5, 1));
+        radioPanel.add(birdButton);
+        radioPanel.add(catButton);
+        radioPanel.add(dogButton);
+        radioPanel.add(rabbitButton);
+        radioPanel.add(pigButton);
+
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(radioPanel, BorderLayout.WEST);
+        getContentPane().add(petLabel, BorderLayout.CENTER);
+    }
+
+    private void displayPet(String pet) {
+        String imagePath = "";
+        switch (pet) {
+            case "Bird":
+                imagePath = "bird.jpg";
+                break;
+            case "Cat":
+                imagePath = "cat.jpg";
+                break;
+            case "Dog":
+                imagePath = "dog.jpg";
+                break;
+            case "Rabbit":
+                imagePath = "rabbit.jpg";
+                break;
+            case "Pig":
+                imagePath = "pig.jpg";
+                break;
+            default:
+                break;
+        }
+        ImageIcon icon = new ImageIcon(getClass().getResource(imagePath));
+        petLabel.setIcon(icon);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new PetDisplayApp().setVisible(true);
+            }
+        });
+    }
+}
    
